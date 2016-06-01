8e39882c80b44a5c8643c9053b9e363fc9c48b73
guru-github <guru.github@gmail.com>
2016-06-02 01:00:12 +0530

    test commit

--- Bgts.Gps.Core/Company/Company.cs
+++ Bgts.Gps.Core/Company/Company.cs
@@ -12,6 +12,7 @@ namespace Bcs.Gps.Core.Company
     [Table("Company")]
     class Company : FullAuditedEntity<long>
     {
+        public string test { get; set; }
         #region Constant Properties
         public const int CompanyNameMaxLength = 64;
         public const int Address1MaxLength = 64;
